c8e1def185ca4ab43ed844a795fab7dbd021e116
LyuboDikov <lyubo.dikov@icloud.com>
2023-09-06 21:23:18 +0300

    Created UserViewModel

---
+++ pathfinder/src/main/java/com/example/pathfinder/models/view/UserViewModel.java
@@ -0,0 +1,12 @@
+package com.example.pathfinder.models.view;
+
+import com.example.pathfinder.models.enums.LevelEnum;
+
+public class UserViewModel {
+
+    private Long id;
+    private String fullName;
+    private String username;
+    private Integer age;
+    private LevelEnum level;
+}
--- pathfinder/src/main/java/com/example/pathfinder/web/UserController.java
+++ pathfinder/src/main/java/com/example/pathfinder/web/UserController.java
@@ -9,10 +9,7 @@ import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
@@ -115,5 +112,10 @@ public class UserController {
         return "redirect:/";
     }
 
+    @GetMapping("/profile/{id}")
+    private String profile(@PathVariable Long id) {
+
+        return "profile";
+    }
 
 }
