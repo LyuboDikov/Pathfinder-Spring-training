5f7757493d397aa07c3518d632a854b12cbd1769
LyuboDikov <lyubo.dikov@icloud.com>
2023-09-07 12:30:29 +0300

    Created Picture repo and service, fixed pictures gallery in index page.

---
+++ pathfinder/src/main/java/com/example/pathfinder/services/impl/PictureServiceImpl.java
@@ -0,0 +1,23 @@
+package com.example.pathfinder.services.impl;
+
+import com.example.pathfinder.repositories.PictureRepository;
+import com.example.pathfinder.services.PictureService;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class PictureServiceImpl implements PictureService {
+
+    private final PictureRepository pictureRepository;
+
+    public PictureServiceImpl(PictureRepository pictureRepository) {
+        this.pictureRepository = pictureRepository;
+    }
+
+
+    @Override
+    public List<String> findAllUrls() {
+        return pictureRepository.findAllUrls();
+    }
+}
