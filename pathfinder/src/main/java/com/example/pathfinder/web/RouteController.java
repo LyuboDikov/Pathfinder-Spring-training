e2eb8c74bc3924ea1daf87b9046caefd9dd31abb
LyuboDikov <lyubo.dikov@icloud.com>
2023-09-12 15:02:18 +0300

    Implemented login in Route Controller

--- pathfinder/src/main/java/com/example/pathfinder/web/RouteController.java
+++ pathfinder/src/main/java/com/example/pathfinder/web/RouteController.java
@@ -1,17 +1,85 @@
 package com.example.pathfinder.web;
 
+import com.example.pathfinder.models.binding.RouteAddBindingModel;
+import com.example.pathfinder.models.service.RouteServiceModel;
+import com.example.pathfinder.services.RouteService;
+import com.example.pathfinder.util.CurrentUser;
+import jakarta.validation.Valid;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.io.IOException;
 
 @Controller
 @RequestMapping("/routes")
 public class RouteController {
 
+    private final RouteService routeService;
+    private final CurrentUser currentUser;
+    private final ModelMapper modelMapper;
+
+    public RouteController(RouteService routeService, CurrentUser currentUser, ModelMapper modelMapper) {
+        this.routeService = routeService;
+        this.currentUser = currentUser;
+        this.modelMapper = modelMapper;
+    }
 
     @GetMapping("/all")
-    public String allRoutes(){
+    public String allRoutes(Model model){
+
+        model.addAttribute("routes", routeService.findAllRoutesViewModels());
 
         return "routes";
+    }
+
+    @GetMapping("/add")
+    public String add() {
+
+        if (currentUser.getId() == null) {
+            return "redirect:/users/login";
+        }
+
+        return "add-route";
+    }
+
+    @PostMapping("/add")
+    public String addConfirm(@Valid RouteAddBindingModel routeAddBindingModel,
+                             BindingResult bindingResult,
+                             RedirectAttributes redirectAttributes) throws IOException {
+
+        if (bindingResult.hasErrors()) {
+
+            redirectAttributes.addFlashAttribute("routeAddBindingModel", routeAddBindingModel);
+            redirectAttributes.addFlashAttribute("org.springframework.validation.BindingResult.routeAddBindingModel",
+                    bindingResult);
+
+            return "redirect:add";
+        }
+
+        RouteServiceModel routeServiceModel = modelMapper.
+                map(routeAddBindingModel, RouteServiceModel.class);
+
+        routeServiceModel.
+                setGpxCoordinates(new String(
+                        routeAddBindingModel.
+                                getGpxCoordinates().
+                                getBytes()));
+
+        routeService.addNewRoute(routeServiceModel);
+
+        return "redirect:all";
+
+    }
+
+    @ModelAttribute
+    public RouteAddBindingModel routeAddBindingModel() {
+        return new RouteAddBindingModel();
     }
 }
